5aaf6bd93316fb49f798d2c82fe41d2abab8ece0
aniek <90182483+aniekM@users.noreply.github.com>
2021-10-05 10:50:32 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+var hamburger = document.querySelector ('.hamburger');
+var menu = document.querySelector ('header nav ul');
+
+
+
+function open(){
+    hamburger.classList.toggle('open');
+    menu.classList.toggle('oop');
+}
+
+hamburger.addEventListener ('click', open);
