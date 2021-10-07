ea49449ecc43fd26cfedb3d3aea7c75189083184
aniek <90182483+aniekM@users.noreply.github.com>
2021-10-07 19:01:27 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 // JavaScript Document
 var hamburger = document.querySelector ('.hamburger');
 var menu = document.querySelector ('header nav ul');
+var button = document.querySelector ('button');
+var header = document.querySelector ('h1');
 
 
 
@@ -9,4 +11,16 @@ function open(){
     menu.classList.toggle('oop');
 }
 
-hamburger.addEventListener ('click', open);
+button.addEventListener ('click', open);
+
+window.onscroll = function(){
+    var top = window.scrollY;
+    console.log(top);
+    if (top >= 60){
+        menu.classList.add('scroll');
+        header.classList.add('scroll');
+    } else{
+        menu.classList.remove('scroll');
+        header.classList.remove('scroll');
+    }
+}
